cfac53fa371476242468c9a3a337191238b0461b
sebi <andreidodot@yahoo.com>
2023-06-20 22:37:45 +0300

    client-server

---
+++ server/server.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <pthread.h>
+#include <sys/socket.h>
+
+#define threadpool_size 4
+
+int main(){
+    pthread_t thread_pool[threadpool_size];
+
+    system("pwd");
+
+}
