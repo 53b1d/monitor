7feed757427d27fba1c4f9cf7f8bd92796d9c40c
sebi <andreidodot@yahoo.com>
2023-06-30 11:05:06 +0300

    improved command input handler

--- server/server.cpp
+++ server/server.cpp
@@ -1,8 +1,68 @@
 #include "server.h"
 
-void process_data(int client_fd) {
-    std::cout << "Processing request for client" << client_fd << "\n";
+server::server()
+{
+    this->server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if(server_fd < 0) {
+        perror("socket");
+        exit(1);
+    }
+
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_address.sin_port = htons(5454);
+
+    this->thread_pool = new threadpool(3);
 }
+
+void server::init()
+{
+    running = true;
+
+    int reuse = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+        perror("setsockopt");
+        exit(1);
+    }
+
+    if(bind(server_fd, (sockaddr*)&server_address, sizeof(server_address)) < 0) {
+        perror("bind");
+        exit(1);
+    }
+
+    char hostname[24];
+    gethostname(hostname, 24);
+
+    if(listen(server_fd, 50) < 0) {
+        perror("listen");
+        exit(1);
+    }
+
+    std::cout << "server: running on " << hostname << " " << htons(server_address.sin_port) << "\n";
+    std::cout << "man --list available commands\n";
+
+    std::thread cli_thread(&server::handle_cli, this);
+    cli_thread.detach();
+}
+
+server::~server() {
+    delete thread_pool;
+    close(server_fd);
+}
+
+void process_data(int client_fd) {
+    char buffer[1024];
+    int num_bytes = recv(client_fd, buffer, sizeof(buffer), 0);
+    if (num_bytes > 0) {
+        std::string received_message(buffer, num_bytes);
+        std::cout << "client " << client_fd << ": " << received_message;
+        rl_forced_update_display();
+    } else {
+        perror("recv");
+    }
+}
+
 
 void server::handle_client(int client_fd) {
     {
@@ -12,7 +72,12 @@ void server::handle_client(int client_fd) {
 
     pollfd poll_fd {client_fd, POLLIN | POLLRDHUP, 0}; //set pollfd for client
 
-    while(running) {
+    while(true) {
+
+        if(running == false) {
+            break;
+        }
+
         //loop until client disconnects 
         int num_ready = poll(&poll_fd, 1, -1);
         if(num_ready < 0) {
@@ -43,7 +108,10 @@ void server::handle_client(int client_fd) {
 }
 
 void server::accept_connections() {
-    while(running) { //loop and accept incoming connections
+    while(true) { //loop and accept incoming connections
+        if(running == false) {
+            break;
+        }
         struct sockaddr_in client_address;
         socklen_t client_address_size = sizeof(client_address);
         int client_fd = accept(server_fd, (sockaddr*)&client_address, &client_address_size);
@@ -60,120 +128,76 @@ void server::accept_connections() {
                 continue;
             }
         }
-
-        if(!running) {
-            close(client_fd);
-            break;
-        }    
-
         thread_pool->enqueue(std::bind(&server::handle_client, this, client_fd)); //handle the new connection
     }
 }
 
 void server::handle_cli() {
     std::string input;
-    while (running) {
+    while(true) {
         input = readline("> ");
         add_history(input.c_str());
-
+        // Parse the command and client
         std::istringstream iss(input);
-        std::vector<std::string> tokens(std::istream_iterator<std::string>{iss}, std::istream_iterator<std::string>());
-
-        if (tokens.empty()) {
-            continue;
-        }
-
-        std::string command = tokens[0];
-
-        if (command == "clients") {
-            // Handle clients command
-            // ...
-        } else if (command == "exit") {
-            // Handle exit command
-            // ...
-        } else if (command == "services") {
-            // Handle services command
-            // ...
-        } else if (command == "inject") {
-            // Handle inject command
-            if (tokens.size() < 3) {
-                std::cout << "Invalid command format. Usage: inject <client> <script-file>" << std::endl;
-                continue;
-            }
-
-            int clientID = std::stoi(tokens[1]);
-            std::string scriptFile = tokens[2];
-
+        std::string command;
+        int client_fd;
+        iss >> command >> client_fd;
+        
+        if(input == "clients") {
+            std::cout << "Connected clients:" << std::endl; //list of connected clients
             {
                 std::lock_guard<std::mutex> lock(clients_mutex);
-
-                if (clientID < 0 || clientID >= clients.size()) {
-                    std::cout << "Invalid client ID" << std::endl;
-                    continue;
+                for (int client : clients) {
+                    struct sockaddr_in addr;
+                    socklen_t addr_len = sizeof(addr);
+                    if (getpeername(client, (struct sockaddr*)&addr, &addr_len) == 0) {
+                        std::cout << "client " << client << ": " << inet_ntoa(addr.sin_addr) << "|" << ntohs(addr.sin_port) << std::endl;
+                    }
                 }
-
-                std::ifstream file("stats.sh");
-                if (!file) {
-                    std::cout << "Script file not found" << std::endl;
-                    continue;
-                }
-
-                std::string scriptContent((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
-
-                // Send the script to the client
-                int client = clients[clientID];
-                send(client, scriptContent.c_str(), scriptContent.size(), 0);
-                std::cout << "Script injected to client " << clientID << std::endl;
-            }
-        } else {
-            std::cout << "Unknown command" << std::endl;
+            } 
         }
-    }
+
+        else if(input == "man") {
+            std::cout << "app <client> <app_name> --downloads and installs an app\n";
+            std::cout << "auths <client> --get auth logs\n";
+            std::cout << "clients <client> --lists connected clients\n";
+            std::cout << "resources <client> --get resources usage\n"; 
+            std::cout << "service <client> <service> --get running services\n"; 
+            std::cout << "services <client> --get running services\n"; 
+            std::cout << "ssh_logs <client> --get ssh logs\n"; 
+            std::cout << "stats <client> --get stats\n"; 
+            std::cout << "users <client> --get system users\n"; 
+            std::cout << "exit --close the server\n";
+        }
+
+        else if(input == "exit") {
+            {
+                //close all client sockets
+                std::lock_guard<std::mutex> lock(clients_mutex);
+                for(int client : clients) {
+                    close(client);
+                }
+            }
+            running = false;
+            delete thread_pool;
+            rl_reset_terminal(NULL);
+            exit(0);
+        }
+
+        else 
+        {
+            send_command(client_fd, command);
+        }
+    } 
 }
 
-
-server::server()
-{
-    this->server_fd = socket(AF_INET, SOCK_STREAM, 0);
-
-    if(server_fd < 0) {
-        perror("socket");
-        exit(1);
-    }
-
-    server_address.sin_family = AF_INET;
-    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
-    server_address.sin_port = htons(5454);
-
-    this->thread_pool = new threadpool(3);
-}
-
-void server::init()
-{
-    running = true;
-
-    if(bind(server_fd, (sockaddr*)&server_address, sizeof(server_address)) < 0) {
-        perror("bind");
-        exit(1);
-    }
-
-    char hostname[24];
-    gethostname(hostname, 24);
-
-    if(listen(server_fd, 50) < 0) {
-        perror("listen");
-        exit(1);
-    }
-
-    std::cout << "server: running on " << hostname << " " << htons(server_address.sin_port) << "\n";
-
-    std::thread cli_thread(&server::handle_cli, this);
-    cli_thread.detach();
-}
-
-server::~server() {
-    delete this->thread_pool;
-    if (!running) {
-        close(server_fd);
+void server::send_command(int client_fd, const std::string& command) {
+    std::lock_guard<std::mutex> lock(clients_mutex);
+    if (std::find(clients.begin(), clients.end(), client_fd) != clients.end()) {
+        // Send the command to the specified client
+        std::string message = command + "\n";
+        send(client_fd, message.c_str(), message.length(), 0);
+    } else {
+        std::cout << "invalid client: " << client_fd << std::endl;
     }
 }
